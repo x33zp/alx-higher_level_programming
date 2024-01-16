4892afcdd806d3478964eaa7faf77ef2b9399932
zubby peculiar <zubbypeculiar@gmail.com>
2024-01-16 18:36:59 +0100

    fixed format string requirement

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,5 +4,5 @@ def uppercase(str):
     for i in str:
         if ord(i) >= 97 and ord(i) <= 123:
             i = chr(ord(i)-32)
-        print(i, end='')
+        print({}.format(i), end='')
     print()
