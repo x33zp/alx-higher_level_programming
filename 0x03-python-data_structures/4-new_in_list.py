48cc6d53a8a5a787eb8ac92811ca9b5d65f9260f
zubby peculiar <zubbypeculiar@gmail.com>
2024-01-20 15:41:18 +0100

    replaces an element in a list at a specific position without modifying the original list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    '''replaces an element in a list at a specific position without modifying the original list'''
+    if idx < 0 or idx >= len(my_list):
+        return list_copy
+    else:
+        list_copy = my_list.copy()
+        list_copy[idx] = element
+        return list_copy
