1195dde815442fa347a4be48a0a914a6ab8acabb
zubby peculiar <zubbypeculiar@gmail.com>
2024-01-20 16:23:23 +0100

    style fix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,5 +6,5 @@ def print_matrix_integer(matrix=[[]]):
             if j == i[-1]:
                 print('{:d}'.format(j), end='')
             else:
-                 print('{:d}'.format(j), end=' ')
+                print('{:d}'.format(j), end=' ')
         print()
