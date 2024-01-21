aa7e18f191575c7964114ea22e5ebfb8dd557f79
zubby peculiar <zubbypeculiar@gmail.com>
2024-01-21 08:40:23 +0100

    varible name fix

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     '''finds the biggest integer of a list.'''
-    max = my_list[0] if len(my_list) else None
+    max_int = my_list[0] if len(my_list) else None
+
     for i in my_list:
-        if i > max:
-            max = i
-    return max
+        if i > max_int:
+            max_int = i
+    return max_int
