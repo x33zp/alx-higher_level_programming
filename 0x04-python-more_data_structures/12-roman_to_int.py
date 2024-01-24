9a6b1dd5b8e21ff64220fcade514434d36f94e13
zubby peculiar <zubbypeculiar@gmail.com>
2024-01-24 23:33:27 +0100

    roman higures

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                      'C': 100, 'D': 500, 'M': 1000}
+
+    total = 0
+    previous = 0
+    for roman in reversed(roman_string)
+        current = roman_numerals[roman]
+        if current < previous:
+            total -= current
+        else:
+            total += current
+        previous = current
+    return total 
+            
