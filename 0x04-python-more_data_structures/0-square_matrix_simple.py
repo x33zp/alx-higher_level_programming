682843c3363128d8ccbe53b6c06bf228d503a9e8
zubby peculiar <zubbypeculiar@gmail.com>
2024-01-24 12:39:31 +0100

    pycodestyle fix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,4 +3,3 @@ def square_matrix_simple(matrix=[]):
     '''Computes the square value of all integers of a matrix'''
     new_matrix = [list(map(lambda x: x**2, i)) for i in matrix]
     return new_matrix
-            
