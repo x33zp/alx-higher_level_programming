bc3ed160c77329e4325dbbbed22264d2904ed762
zubby peculiar <zubbypeculiar@gmail.com>
2024-01-25 00:21:23 +0100

    code fix

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    '''returns the weighted average of all integers tuple'''
+    if my_list:
+        
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,6 +3,9 @@ def roman_to_int(roman_string):
     roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
                       'C': 100, 'D': 500, 'M': 1000}
 
+    if not roman_string or type(roman_string) is not str:
+        return 0
+
     total = 0
     previous = 0
     for roman in reversed(roman_string):
