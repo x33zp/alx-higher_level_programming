7954df388f755503a76aed5abf2dd270d047a126
zubby peculiar <zubbypeculiar@gmail.com>
2024-02-09 12:06:09 +0100

    fix

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+
+"""Defines a square"""
+
 class Square:
     """A class Square that defines a square with private instance attribute: size"""
-    def __init__(self, _Square__size):
+    def __init__(self, size):
         """Initialize an instance: size of class: Square"""
-        self._Square__size = _Square__size
+        self._Square__size = size
