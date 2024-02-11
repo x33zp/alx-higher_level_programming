eb5fb0e2bca3c9a15b1fe085e7551ff1dac6723a
zubby peculiar <zubbypeculiar@gmail.com>
2024-02-11 07:50:47 +0100

    code fix

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -92,6 +92,7 @@ class Square:
         """
         if self.__size == 0:
             print()
+            return
         for j in range(self.__position[1]):
             print()
         for i in range(self.__size):
