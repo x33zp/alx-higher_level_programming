faf45f7bf47a3511d07c6660119b1d35f43b545f
zubby peculiar <zubbypeculiar@gmail.com>
2024-02-15 20:47:54 +0100

    final commit

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -22,8 +22,8 @@ class MagicClass:
             TypeError: If radius is not a number.
         """
         self.__radius = 0
-        if not isinstance(radius, (int, float)):
-            TypeError("radius must be a number")
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
         self.__radius = radius
 
     def area(self):
