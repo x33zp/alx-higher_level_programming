07e960d92b16be4b2e76570452d964f5bfcdeafb
zubby peculiar <zubbypeculiar@gmail.com>
2024-02-15 06:23:30 +0100

    fix

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -100,7 +100,8 @@ class Rectangle:
         return 2 * (self.__width + self.__height)
 
     def __str__(self):
-        return "\n".join([str(Rectangle.print_symbol) * self.width] * self.height)
+        return "\n".join([
+            str(Rectangle.print_symbol) * self.__width] * self.__height)
 
     def __repr__(self):
         return f"Rectangle({self.width}, {self.height})"
@@ -111,11 +112,12 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
+        """Returns the biggest rectangle based on the area"""
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
-        
+
         if rect_1.area() >= rect_2.area():
             return rect_1
         else:
