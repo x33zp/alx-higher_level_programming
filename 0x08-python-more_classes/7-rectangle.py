3a7e1a0c5230eea151cda2eb2c10783a053e2f6b
zubby peculiar <zubbypeculiar@gmail.com>
2024-02-15 06:24:41 +0100

    code style fix

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -103,7 +103,8 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ""
 
-        return "\n".join([str(self.print_symbol) * self.__width] * self.__height)
+        return "\n".join([
+            str(self.print_symbol) * self.__width] * self.__height)
 
     def __repr__(self):
         return f"Rectangle({self.width}, {self.height})"
