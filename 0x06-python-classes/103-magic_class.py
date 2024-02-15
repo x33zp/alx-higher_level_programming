d2bf553b03643f2ecceb4943d54a3fbb635cc42e
zubby peculiar <zubbypeculiar@gmail.com>
2024-02-15 20:37:34 +0100

    initial commit

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,11 @@
+class MagicClass:
+    def__init__(self, radius=0):
+        if not isinstance(radius, (int, float)):
+            TypeError("radius must be a number")
+        self.__radius = radius
+
+    def area(self):
+        return math.pi * self.__radius ** 2
+
+    def circumference(self):
+        return 2 * math.pi * self.__radius
