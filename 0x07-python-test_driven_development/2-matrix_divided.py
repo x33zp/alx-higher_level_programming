8be703e84a69cfc31eeaf4f4312b82f8be148c34
zubby peculiar <zubbypeculiar@gmail.com>
2024-02-16 14:48:14 +0100

    fix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -30,7 +30,7 @@ def matrix_divided(matrix, div):
     elif any(len(row) != len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
 
-    if not isinstance(div, int):
+    if not isinstance(div, int) and not isinstance(div, float):
         raise TypeError("div must be a number")
     elif div == 0:
         raise ZeroDivisionError("division by zero")
