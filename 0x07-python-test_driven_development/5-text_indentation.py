1658bef9c5d18c99653a9edae816b570e222ab80
zubby peculiar <zubbypeculiar@gmail.com>
2024-02-17 18:52:32 +0100

    please work

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -6,18 +6,19 @@ Supplies a function that indents text
 
 def text_indentation(text):
     """
-    prints a text with 2 new lines after each of these characters: '.', '?' and ':'
+    prints a text with 2 new lines after each of these characters:
+    '.', '?' and ':'
     """
     if type(text) is not str:
         raise TypeError("text must be a string")
-    charID = 0
+    count = 0
     for char in text:
-        if charID == 0:
+        if count == 0:
             if char == ' ':
                 continue
             else:
-                charID = 1
-        if charID == 1:
+                count = 1
+        if count == 1:
             if char == '?' or char == '.' or char == ':':
                 print(char)
                 print()
