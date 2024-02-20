3a0edccee98026f8e9045c395ac9084f060678c3
zubby peculiar <zubbypeculiar@gmail.com>
2024-02-20 12:22:00 +0100

    an empty class

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+A class BaseGeometry
+"""
+
+class BaseGeometry:
+    """
+    An empty class representing base geometry.
+
+    This class serves as a base class for geometry-related classes.
+    """
+    pass
