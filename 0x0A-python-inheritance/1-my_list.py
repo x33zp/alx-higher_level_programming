b2e89d701780837c12d898950e4bfacd435cc4c7
zubby peculiar <zubbypeculiar@gmail.com>
2024-02-20 09:54:56 +0100

    a class MyList that inherits from list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+contains MyList class
+"""
+
+
+class MyList(list):
+    """
+    A subclass of list that provides additional functionality.
+    """
+    def print_sorted(self):
+        """
+        Prints the list in sorted order (ascending).
+
+        This method sorts the list in ascending order and prints it.
+        """
+        print(sorted(self))
+        
