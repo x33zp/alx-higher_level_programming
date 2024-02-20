5a1d90fb233a3f7a09e6e20fefdf3a80908ebfd5
zubby peculiar <zubbypeculiar@gmail.com>
2024-02-20 09:29:56 +0100

    a function that returns the list of available attributes and methods of an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Contains a function that looks up attributes of obj
+"""
+
+
+def lookup(obj):
+    """Returns a list available atrributes and methods"""
+    return dir(obj)
+    
