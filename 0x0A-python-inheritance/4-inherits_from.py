10f8610d4acb6b7406e19b8ec36a01c30ac7bcb7
zubby peculiar <zubbypeculiar@gmail.com>
2024-02-20 12:08:55 +0100

    code style fix

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -18,4 +18,4 @@ def inherits_from(obj, a_class):
               specified class, False otherwise.
     """
 
-    return issubclass(type(obj), a_class) and type(obj) != a_class
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
