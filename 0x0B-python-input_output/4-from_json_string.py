f8a5a51f5887a9b32430af25a3e18b95f483a4e3
zubby peculiar <zubbypeculiar@gmail.com>
2024-02-21 04:16:19 +0100

    converts from json to py object

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Contains function from_json_string
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    Converts a JSON string `my_str` into a Python object.
+
+    Parameters:
+        my_str (str): The JSON string to be converted into a Python object.
+
+    Returns:
+        any: The Python object representation of the input JSON string `my_str`.
+
+    Note:
+        This function internally uses the `json.loads()` function from the
+        `json` module to perform the conversion.
+    """
+    return json.loads(my_str)
