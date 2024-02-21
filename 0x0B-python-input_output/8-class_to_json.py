511eaf4dfb14506087aaba6be7b27285efa1fd6e
zubby peculiar <zubbypeculiar@gmail.com>
2024-02-21 06:02:09 +0100

    fix(): style

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -15,13 +15,11 @@ def class_to_json(obj):
 
     Note:
         This function assumes that the object has a `__dict__` attribute, which
-        contains
-        all the attributes and their values of the object. It directly returns this
-        dictionary,
-        which may include non-serializable attributes.
+        contains all the attributes and their values of the object. It directly
+        returns this dictionary, which may include non-serializable attributes.
 
-        It's recommended to use this function only when you're sure that all attributes of
-        the object are serializable (i.e., they are of types list, dictionary, string,
-        integer, or boolean).
+        It's recommended to use this function only when you're sure that all
+        attributes of the object are serializable (i.e., they are of types list
+        dictionary, string integer, or boolean).
     """
     return obj.__dict__
