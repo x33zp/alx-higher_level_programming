a6ef2ae82933aead2435837ec00fa79e35a677d8
zubby peculiar <zubbypeculiar@gmail.com>
2024-02-21 05:01:13 +0100

    fix(): style

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -11,11 +11,13 @@ def save_to_json_file(my_obj, filename):
 
     Parameters:
         my_obj (any): The Python object to be serialized and saved to the file.
-        filename (str): The name of the file to which the JSON representation will be saved.
+        filename (str): The name of the file to which the JSON representation
+        will be saved.
 
     Note:
-        This function uses the `json.dump()` function from the `json` module to serialize
-        the input Python object `my_obj` into a JSON string and save it to the specified file.
+        This function uses the `json.dump()` function from the `json` module to
+        serialize the input Python object `my_obj` into a JSON string and save
+        it to the specified file.
     """
     with open(filename, 'w') as file:
         json.dump(my_obj, file)
