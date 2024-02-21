e420609ecf7511cb409c115df2ec67673ef24eae
zubby peculiar <zubbypeculiar@gmail.com>
2024-02-21 03:51:59 +0100

    returns the JSON representation of an object (string):

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Contains a function to_json_string
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Converts a Python object `my_obj` into a JSON string.
+
+    Parameters:
+        my_obj (any): The Python object to be converted to a JSON string.
+
+    Returns:
+        str: The JSON representation of the input Python object `my_obj`.
+
+    Note:
+        This function internally uses the `json.dumps()` function from the
+        `json` module to perform the conversion.
+    """
+    json.dumps(my_obj)
