4276cfcd0e27e7fab61bd87e2732a15b82e58b31
zubby peculiar <zubbypeculiar@gmail.com>
2024-02-21 03:22:18 +0100

    fix(): code expectation fix

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,6 +5,12 @@ Contains function write_file
 
 
 def write_file(filename="", text=""):
+    """
+    writes a string to a text file (UTF8)
+
+    Returns:
+            number of characters written
+    """
     with open(filename, mode='w', encoding='utf-8') as file:
         file.write(text)
-        return len(text)
+    return len(text)
