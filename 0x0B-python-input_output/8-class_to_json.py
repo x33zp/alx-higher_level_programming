06512503cb067ac996eea7cde907761fc8eabe3b
zubby peculiar <zubbypeculiar@gmail.com>
2024-02-21 05:59:49 +0100

    returns the dictionary description

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -6,18 +6,22 @@ contains function class_to_json
 
 def class_to_json(obj):
     """
-    Returns a dictionary representation of the object suitable for JSON serialization.
+    Returns a dictionary representation of the object suitable for
+    JSON serialization.
 
     Returns:
-        dict: A dictionary representing the object with serializable attributes.
+        dict: A dictionary representing the object with serializable
+        attributes.
 
     Note:
-        This function assumes that the object has a `__dict__` attribute, which contains
-        all the attributes and their values of the object. It directly returns this dictionary,
+        This function assumes that the object has a `__dict__` attribute, which
+        contains
+        all the attributes and their values of the object. It directly returns this
+        dictionary,
         which may include non-serializable attributes.
 
-        It's recommended to use this function only when you're sure that all attributes of 
-        the object are serializable (i.e., they are of types list, dictionary, string, 
+        It's recommended to use this function only when you're sure that all attributes of
+        the object are serializable (i.e., they are of types list, dictionary, string,
         integer, or boolean).
     """
     return obj.__dict__
