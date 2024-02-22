579d7faaa8c6b35b0449fdaae47bd65067c2490a
zubby peculiar <zubbypeculiar@gmail.com>
2024-02-22 05:21:52 +0100

    operators inverted

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Defines a class MyInt
+"""
+
+
+class MyInt(int):
+    """
+    A class representing a rebellious integer.
+
+    This class inherits from int but inverts the behavior of the equality and inequality operators.
+    """
+
+    def __eq__(self, other):
+        """
+        Overrides the equality operator (==) to return the inverse of the int's equality check.
+
+        Parameters:
+            other (Any): The value to compare with.
+
+        Returns:
+            bool: True if not equal, False otherwise.
+        """
+        return not super().__eq__(other)
+
+    def __ne__(self, other):
+        """
+        Overrides the inequality operator (!=) to return the inverse of the int's inequality check.
+
+        Parameters:
+            other (Any): The value to compare with.
+
+        Returns:
+            bool: True if equal, False otherwise.
+        """
+        return not super().__ne__(other)
