23867c6fae44037b3ec65808f6f83b96c7f6c097
zubby peculiar <zubbypeculiar@gmail.com>
2024-02-25 07:52:34 +0100

    fix(docsting): added docstring

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -27,14 +27,14 @@ class Square(Rectangle):
     @property
     def size(self):
         """
-        
+        Getter method for the size attribute.
         """
         return self.width
 
     @size.setter
     def size(self, value):
         """
-        
+        Setter method for the size attribute.
         """
         self.width = value
-        self.height = value
+        self.height = value 
