4a8a6e676955a722a410ddb46e402820b2933a87
zubby peculiar <zubbypeculiar@gmail.com>
2024-02-25 04:12:52 +0100

    added(update): a public method that assigns an argument to each attribute

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -148,3 +148,22 @@ class Rectangle(Base):
                                                 self.id, self.__x,
                                                 self.__y, self.__width,
                                                 self.__height)
+
+    def update(self, *args):
+        """
+        Update the attributes of the rectangle using the provided arguments.
+
+        Parameters:
+            args: The arguments to assign to the attributes in the order
+            (id, width, height, x, y).
+        """
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4]
