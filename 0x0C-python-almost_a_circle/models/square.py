ecfd612bc24abf7b7a8b9aabc35f69359aad8fb2
zubby peculiar <zubbypeculiar@gmail.com>
2024-02-25 08:39:34 +0100

    added(to_dictionary): a public method that returns the dictionary representation of the Square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 This module defines the Square class,
-which represents a Square that inherits 
+which represents a Square that inherits
 from Rectangle class.
 """
 
@@ -18,9 +18,9 @@ class Square(Rectangle):
 
         Parameters:
             size: The size of the square (same as width and height).
-            x: The x-coordinate of the top-left corner of the square (default 0).
-            y: The y-coordinate of the top-left corner of the square (default 0).
-            id: Optional. The id to assign to the square (default None).
+            x: The x-coordinate of the top-left corner of the square.
+            y: The y-coordinate of the top-left corner of the square.
+            id: Optional. The id to assign to the square.
         """
         super().__init__(size, size, x, y, id)
 
@@ -37,4 +37,34 @@ class Square(Rectangle):
         Setter method for the size attribute.
         """
         self.width = value
-        self.height = value 
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """
+        Update the attributes of the square using the provided
+        arguments.
+
+        Parameters:
+            args: Positional arguments representing the attributes
+            of the square in the order (id, size, x, y).
+            kwargs: Keyword arguments representing the attributes
+            of the square as key-value pairs.
+        """
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+    def to_dictionary(self):
+        """
+        Resturns a representation of the Square
+        """
+        return dict(id=self.id, x=self.x, size=self.size, y=self.y)
