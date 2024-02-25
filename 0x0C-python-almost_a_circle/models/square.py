53e0d9eb482f8eb097ea2d57a2a06dcc352b9877
zubby peculiar <zubbypeculiar@gmail.com>
2024-02-25 07:17:11 +0100

    added(square) that inherits from Rectangle class Implemented the Square class in models/square.py, inheriting attributes and methods from the Rectangle class. The Square class represents a special type of rectangle where width and height are equal.

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This module defines the Square class,
+which represents a Square that inherits 
+from Rectangle class.
+"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    Represents a square.
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+         Initializes a square with the given size, x, y, and id.
+
+        Parameters:
+            size: The size of the square (same as width and height).
+            x: The x-coordinate of the top-left corner of the square (default 0).
+            y: The y-coordinate of the top-left corner of the square (default 0).
+            id: Optional. The id to assign to the square (default None).
+        """
+        super().__init__(size, size, x, y, id)
