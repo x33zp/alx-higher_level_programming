0800fd540f5f2657be2aa0b7a5777661ed86f964
zubby peculiar <zubbypeculiar@gmail.com>
2024-02-25 05:05:32 +0100

    updated(update): changed the prototype to update(self, *args, **kwargs) that assigns a key/value argument to attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -156,14 +156,21 @@ class Rectangle(Base):
         Parameters:
             args: The arguments to assign to the attributes in the order
             (id, width, height, x, y).
+        kwargs: Keyword arguments representing the attributes of the
+            rectangle as key-value pairs.
         """
-        if len(args) >= 1:
-            self.id = args[0]
-        if len(args) >= 2:
-            self.width = args[1]
-        if len(args) >= 3:
-            self.height = args[2]
-        if len(args) >= 4:
-            self.x = args[3]
-        if len(args) >= 5:
-            self.y = args[4]
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.width = args[1]
+            if len(args) >= 3:
+                self.height = args[2]
+            if len(args) >= 4:
+                self.x = args[3]
+            if len(args) >= 5:
+                self.y = args[4]
+
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
