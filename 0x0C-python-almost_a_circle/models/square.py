7195f9563e8af9b3ac455bd0a15fdf199f21c866
zubby peculiar <zubbypeculiar@gmail.com>
2024-02-26 20:42:49 +0100

    fix(): pycodestyle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -32,8 +32,8 @@ class Square(Rectangle):
             [Rectangle] (<id>) <x>/<y> - <width>/<height>
         """
         return '[{}] ({}) {}/{} - {}'.format(type(self).__name__,
-                                                self.id, self.x,
-                                                self.y, self.size)
+                                             self.id, self.x,
+                                             self.y, self.size)
 
     @property
     def size(self):
