6b1f022de47e0200190f5dd7c18f667e3d508e04
zubby peculiar <zubbypeculiar@gmail.com>
2024-02-28 16:15:53 +0100

    a pointer to a function

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -6,6 +6,6 @@ def safe_function(fct, *args):
     try:
         result = fct(*args)
         return result
-    except:
+    except (ZeroDivisionError, IndexError):
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return None
