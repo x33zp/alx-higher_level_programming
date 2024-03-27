c286455b4d6134d0b0be89844835965136e29dc8
zubby peculiar <zubbypeculiar@gmail.com>
2024-03-27 23:06:38 +0100

    lists all shows contained in the database hbtn_0d_tvshows

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,5 @@
+-- Lists all shows contained in the database hbtn_0d_tvshows.
+SELECT ts.title, tsg.genre_id
+    FROM tv_shows ts LEFT OUTER JOIN tv_show_genres tsg
+        ON ts.id = tsg.show_id
+    ORDER BY ts.title, tsg.genre_id;
