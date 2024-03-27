c1aa3c7f35159bb923d59cb087fc614b56257a5c
zubby peculiar <zubbypeculiar@gmail.com>
2024-03-27 22:56:40 +0100

    lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,5 @@
+-- Lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+SELECT ts.title, tsg.genre_id
+    FROM tv_shows ts INNER JOIN tv_show_genres tsg
+        ON ts.id = tsg.show_id
+    ORDER BY ts.title, tsg.genre_id;
