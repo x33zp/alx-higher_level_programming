bb0d8c7e3c5dd0f0296944d5ddbffcef89e90a95
zubby peculiar <zubbypeculiar@gmail.com>
2024-03-28 17:38:58 +0100

    fix

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,10 +1,12 @@
--- A script that lists all genres in the database hbtn_0d_tvshows_rate by their rating.
-SELECT g.name, SUM(r.rate) as rating
-FROM tv_genres AS g
-INNER JOIN tv_show_genres AS s
-ON g.id = s.genre_id
+-- lists all genres in the database hbtn_0d_tvshows_rate by their rating.
+-- Each record should display: tv_genres.name - rating sum
+-- Results must be sorted in ascending order by their rating
 
-INNER JOIN tv_show_ratings as r
-ON r.show_id = s.show_id
-GROUP BY g.name
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show_ratings
+ON tv_show_genres.show_id = tv_show_ratings.show_id
+GROUP BY tv_genres.id
 ORDER BY rating DESC;
