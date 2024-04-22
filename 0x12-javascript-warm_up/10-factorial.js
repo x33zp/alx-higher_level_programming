65e30402680bdba2d2eea8318c209298d7d48dad
zubby peculiar <zubbypeculiar@gmail.com>
2024-04-22 12:51:52 +0100

    fix

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 
-factorial = (n) => {
+function factorial (n) {
   return isNaN(n) || n === 0 ? 1 : n * factorial(n - 1);
-};
+}
 
 console.log(factorial(Number(argv[2])));
