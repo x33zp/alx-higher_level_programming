694eee69982f722afc5b249940479d068468783b
zubby peculiar <zubbypeculiar@gmail.com>
2024-04-23 17:34:35 +0100

    fix

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,7 +4,7 @@ const Square1 = require('./5-square');
 class Square extends Square1 {
   charPrint (c) {
     for (let i = 0; i < this.width; i++) {
-      c === undefined ? console.log('X'.repeat(this.width)) : console.log('C'.repeat(this.width));
+      c === undefined ? console.log('X'.repeat(this.width)) : console.log(c.repeat(this.width));
     }
   }
 }
