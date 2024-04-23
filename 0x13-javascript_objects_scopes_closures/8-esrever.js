6db5976bb3fe21b32d1ceeaea1c6bb37d526cf22
zubby peculiar <zubbypeculiar@gmail.com>
2024-04-23 17:56:24 +0100

    fix

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  reversedList = []
+  const reversedList = [];
   for (let i = 0; i < list.length; i++) {
-    reversedList.unshift(list[i])
+    reversedList.unshift(list[i]);
   }
-  return reversedList
+  return reversedList;
 };
