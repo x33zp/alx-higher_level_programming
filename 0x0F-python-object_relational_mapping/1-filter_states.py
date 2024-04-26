cea64df1e812621c2fc4b867d541406ec20c86ae
zubby peculiar <zubbypeculiar@gmail.com>
2024-04-27 00:14:34 +0100

    fix

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,17 +5,18 @@ from the 'states' table, then prints all states with a
 name starting with N (upper N) from the database
 """
 import MySQLdb
-import sys
+from sys import argv
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(
-         host='localhost',
-         user=sys.argv[1],
-         passwd=sys.argv[2],
-         db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' \
-                ORDER BY states.id")
-    rows = cur.fetchall()
-    for row in rows:
+    db_connect = db.connect(host="localhost", port=3306,
+                            user=argv[1], passwd=argv[2], db=argv[3])
+    db_cursor = db_connect.cursor()
+
+    db_cursor.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY 'N%' \
+                ORDER BY states.id ASC")
+
+    rows_selected = db_cursor.fetchall()
+
+    for row in rows_selected:
         print(row)
