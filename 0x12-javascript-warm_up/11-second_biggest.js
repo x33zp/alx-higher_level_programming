59fa9bb93862fbb61b4282c66745a45d477a8d21
x33zp <zubbypeculiar@gmail.com>
2024-06-19 08:50:27 -0700

    fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
+let sortedArgs;
 
 if (args === undefined || args.length < 2) {
   console.log(0);
 } else {
-  const sortedArgs = args.sort();
-  console.log(sortedArgs[sortedArgs.length - 2]);
+  sortedArgs = args.sort();
 }
+
+console.log(sortedArgs[sortedArgs.length - 2]);
