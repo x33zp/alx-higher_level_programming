4ca54565b6cdc77530d47eb26a53b2d2bd4815fb
x33zp <zubbypeculiar@gmail.com>
2024-06-19 05:27:19 -0700

    fifth task

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const args = process.argv
-const text1 = args[2]
-const text2 = args[3]
+const args = process.argv;
+const text1 = args[2];
+const text2 = args[3];
 
-console.log(`${text1} is ${text2}`)
+console.log(`${text1} is ${text2}`);
