71e9a8c15d03e7e823411e81a71fb13583203ab0
x33zp <zubbypeculiar@gmail.com>
2024-06-19 04:59:40 -0700

    fix

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const args = process.argv
+const args = process.argv;
 
-if(args.length < 3) {
-  console.log('No argument')
+if (args.length < 3) {
+  console.log('No argument');
 } else {
-    console.log(args[2])
+  console.log(args[2]);
 }
