e30cb6c423fd863ebbb516c3b2c7c50dc14bbcb1
x33zp <zubbypeculiar@gmail.com>
2024-06-19 07:16:28 -0700

    fix

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-const xValue = process.argv[2]
-const textToPrint = 'C is fun'
+const xValue = process.argv[2];
+const textToPrint = 'C is fun';
 
 if (xValue === undefined) {
-  console.log('Missing number of occurrences')
+  console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < xValue; i++) {
-    console.log(textToPrint)
+    console.log(textToPrint);
   }
 }
