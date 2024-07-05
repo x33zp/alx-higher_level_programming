2e139ffe43983c88c07d0c7a83485d9fe594a2ad
x33zp <zubbypeculiar@gmail.com>
2024-07-05 07:04:59 -0700

    fetch a url with urllib

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# a py script that fetches a url
+
+import urllib.request
+
+if __name__ == "__main__":
+    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
+        page = response.read()
+        print('Body response')
+        print('\t- type: {}'.format(type(page)))
+        print('\t- content: {}'.format(page))
+        print('\t- utf8 content: {}'.format(page.decode('utf-8')))
+
