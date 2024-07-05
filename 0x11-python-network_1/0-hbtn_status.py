c99a655f225997d435f54386603b70629a20695f
x33zp <zubbypeculiar@gmail.com>
2024-07-05 07:16:15 -0700

    fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 """a py script that fetches a url"""
-import urllib.request
 
 
 if __name__ == "__main__":
+
+    import urllib.request
+
     req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
     with urllib.request.urlopen(req) as response:
         page = response.read()
+
         print('Body response')
         print('\t- type: {}'.format(type(page)))
         print('\t- content: {}'.format(page))
