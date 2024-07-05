f2667c82d43f4fc91914a869bfa7179416592c20
x33zp <zubbypeculiar@gmail.com>
2024-07-05 08:11:58 -0700

    fix-1

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-"""takes a url and email, sends POST request with an
-email param and displays a body of the response
+"""send a post req with email as param
 """
+
 if __name__ == "__main__":
+
     import urllib.request as req
     import urllib.parse as ps
     import sys
 
     url = sys.argv[1]
-    data = {"email": sys.argv[2]}
-    url_values = ps.urlencode(data).encode("ascii")
+    values = {'email': sys.argv[2]}
 
+    data = ps.urlencode(values)
+    data = data.encode('ascii')
     request = req.Request(url, data)
-
     with req.urlopen(request) as response:
-        print(response.read().decode("utf-8"))
+        print(response.read().decode('utf-8'))
