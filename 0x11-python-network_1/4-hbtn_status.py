11066589174dc736edab4f5b4094cd18170b8193
x33zp <zubbypeculiar@gmail.com>
2024-07-05 20:18:47 -0700

    fetches a url using 'requests' package

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""fetches a url use 'requests' package
+"""
+import requests
+
+r = requests.get('https://alx-intranet.hbtn.io/status')
+
+print('Body response:')
+print('\t- type: {}'.format(type(r.text)))
+print('\t- content: {}'.format(r.text))
