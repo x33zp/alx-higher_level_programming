1b8cf8244e9f3a576587abb4532188ddbcb90354
x33zp <zubbypeculiar@gmail.com>
2024-07-05 20:52:21 -0700

    fix

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -5,6 +5,7 @@
 import requests
 import sys
 
-r = requests.get(sys.argv[1])
+if __name__ == "__main__":
+    r = requests.get(sys.argv[1])
 
-print(r.headers.get('X-Request-Id'))
+    print(r.headers.get('X-Request-Id'))
--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -4,6 +4,7 @@
 import requests
 import sys
 
-r = requests.post(sys.argv[1], data={'email': sys.argv[2]})
+if __name__ == "__main__":
+    r = requests.post(sys.argv[1], data={'email': sys.argv[2]})
 
-print(r.text)
+    print(r.text)
