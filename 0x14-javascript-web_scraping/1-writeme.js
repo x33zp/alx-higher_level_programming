8513522c507a871378ba1956b68d5d1701677c6e
x33zp <zubbypeculiar@gmail.com>
2024-08-11 05:52:07 +0100

    semistandard fix

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-const content = process.argv[3]
+const content = process.argv[3];
 
 fs.writeFile(process.argv[2], content, err => {
   if (err) {
     console.error(err);
-    return;
   }
 });
